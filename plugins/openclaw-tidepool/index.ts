6f77d431e384bc88511c20e3e845952d1a18ab2d
Chad Kirby <chadjkirby@icloud.com>
2026-03-18 20:12:30 -0700

    fix errors in plugin boot

--- plugins/openclaw-tidepool/index.ts
+++ plugins/openclaw-tidepool/index.ts
@@ -1,13 +1,17 @@
-import { defineChannelPluginEntry } from "openclaw/plugin-sdk/core";
 import { tidepoolPlugin, setTidepoolRuntime } from "./src/channel.js";
 
 export { tidepoolPlugin } from "./src/channel.js";
 export { setTidepoolRuntime } from "./src/runtime.js";
 
-export default defineChannelPluginEntry({
+export default {
   id: "tidepool",
   name: "Tidepool",
   description: "SpacetimeDB agent coordination channel plugin",
-  plugin: tidepoolPlugin,
-  setRuntime: setTidepoolRuntime,
-});
+  register(api: {
+    runtime: Parameters<typeof setTidepoolRuntime>[0];
+    registerChannel: (registration: { plugin: typeof tidepoolPlugin }) => void;
+  }) {
+    setTidepoolRuntime(api.runtime);
+    api.registerChannel({ plugin: tidepoolPlugin });
+  },
+};
--- plugins/openclaw-tidepool/setup-entry.ts
+++ plugins/openclaw-tidepool/setup-entry.ts
@@ -1,4 +1,3 @@
-import { defineSetupPluginEntry } from "openclaw/plugin-sdk/core";
 import { tidepoolPlugin } from "./src/channel.js";
 
-export default defineSetupPluginEntry(tidepoolPlugin);
+export default { plugin: tidepoolPlugin };
